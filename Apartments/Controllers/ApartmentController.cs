6df4b488249a7245476eebb4e628845ad43425e1
Ante Bašković <abaskov@algebra.hr>
2023-11-24 15:21:05 +0100

    Add project files.

---
+++ Apartments/Controllers/ApartmentController.cs
@@ -0,0 +1,116 @@
+﻿using System.Web.Mvc;
+using System.Data.Entity;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Web;
+
+namespace Apartments.Controllers
+{
+    public class ApartmentController : Controller
+    {
+        // GET: Apartment
+
+        private readonly ModelContainer db = new ModelContainer();
+
+        ~ApartmentController()
+        {
+            db.Dispose();
+        }
+        public ActionResult Index()
+        {
+            return View(db.Apartments);
+        }
+
+        // GET: Apartment/Details/5
+        public ActionResult Details(int? id)
+        {
+            return CommandAction(id);
+        }
+
+        private ActionResult CommandAction(int? id)
+        {
+            if (id==null)
+            {
+
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            var apartman = db.Apartments
+                .Include(a=>a.UploadedFiles)
+                .SingleOrDefault(a=> a.IDApartment== id);
+
+            if (apartman==null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(apartman);
+        }
+
+        // GET: Apartment/Create
+       
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Apartment apartment, IEnumerable<HttpPostedFileBase> files)
+        {
+            if (ModelState.IsValid)
+            {
+                apartment.UploadedFiles = new List<UploadedFile>();
+                db.Apartments.Add(apartment);
+                db.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Apartment/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            return CommandAction(id);
+
+        }
+
+        // POST: Apartment/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id)
+        {
+            var apartment = db.Apartments.Find(id); 
+            if (TryUpdateModel(apartment,"",new string[]
+            {
+                nameof(Apartment.Address),
+                nameof(Apartment.City),
+                nameof(Apartment.Contact)
+            }))
+            {
+                db.Entry(apartment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(apartment);
+        }
+
+        // GET: Apartment/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            return CommandAction(id);
+
+        }
+
+        // POST: Apartment/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            db.UploadedFiles.RemoveRange(db.UploadedFiles.Where(f=>f.ApartmentIDApartment==id));
+            db.Apartments.Remove(db.Apartments.Find(id));
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Apartments/Model.edmx.sql
@@ -0,0 +1,86 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 11/24/2023 14:38:47
+-- Generated from EDMX file: D:\Podaci\office10\Documents\Apartments\Apartments\Model.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [app];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Apartments'
+CREATE TABLE [dbo].[Apartments] (
+    [IDApartment] int IDENTITY(1,1) NOT NULL,
+    [Address] nvarchar(50)  NOT NULL,
+    [City] nvarchar(20)  NOT NULL,
+    [Contact] nvarchar(50)  NOT NULL
+);
+GO
+
+-- Creating table 'UploadedFiles'
+CREATE TABLE [dbo].[UploadedFiles] (
+    [IDUploadedFile] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(50)  NOT NULL,
+    [ContentType] nvarchar(50)  NOT NULL,
+    [Content] varbinary(max)  NOT NULL,
+    [ApartmentIDApartment] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [IDApartment] in table 'Apartments'
+ALTER TABLE [dbo].[Apartments]
+ADD CONSTRAINT [PK_Apartments]
+    PRIMARY KEY CLUSTERED ([IDApartment] ASC);
+GO
+
+-- Creating primary key on [IDUploadedFile] in table 'UploadedFiles'
+ALTER TABLE [dbo].[UploadedFiles]
+ADD CONSTRAINT [PK_UploadedFiles]
+    PRIMARY KEY CLUSTERED ([IDUploadedFile] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [ApartmentIDApartment] in table 'UploadedFiles'
+ALTER TABLE [dbo].[UploadedFiles]
+ADD CONSTRAINT [FK_ApartmentUploadedFile]
+    FOREIGN KEY ([ApartmentIDApartment])
+    REFERENCES [dbo].[Apartments]
+        ([IDApartment])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_ApartmentUploadedFile'
+CREATE INDEX [IX_FK_ApartmentUploadedFile]
+ON [dbo].[UploadedFiles]
+    ([ApartmentIDApartment]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
